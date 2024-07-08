5f1d560df8d791daa0eac24982e49732c04285d7
rudinengineer <114669073+rudinengineer@users.noreply.github.com>
2024-07-08 19:20:43 +0700

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseurl = 'http://localhost:3000/api'
+const baseurl = 'https://social-media-apalah.vercel.app/'
 
 export const baseAxios = axios.create({
     baseURL: baseurl
