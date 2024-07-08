8c9e440bddbf8dd88198a59a548cdd29f43e5d1f
rudinengineer <114669073+rudinengineer@users.noreply.github.com>
2024-07-08 18:46:45 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,24 @@
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 import { AUTH_PATH, PROTECTED_PATH, PUBLIC_PATH, ROOT_PATH } from "./lib/route"
+import { authOptions } from "@/lib/auth";
 
 // export const config = { matcher: ["/dashboard/", "/dashboard/:path*"] };
+export default async function Middleware() {
+    const session = await getServerSession(authOptions)
 
-export default withAuth(
+    if ( AUTH_PATH.includes(req.nextUrl.pathname) && !session?.user ) {
+            return NextResponse.redirect(new URL('/signin', process.env.NEXT_BASEURL))
+        }
+        if ( PROTECTED_PATH.includes(req.nextUrl.pathname) ) {
+            if ( session?.user ) {
+                return NextResponse.redirect(new URL(ROOT_PATH, process.env.NEXT_BASEURL))
+            }
+        }
+        return NextResponse.next()
+}
+
+/*export default withAuth(
     function middleware(req) {
         if ( AUTH_PATH.includes(req.nextUrl.pathname) && !req.nextauth.token ) {
             return NextResponse.redirect(new URL('/signin', process.env.NEXT_BASEURL))
@@ -25,4 +39,4 @@ export default withAuth(
     {
         secret: process.env.NEXT_AUTH_SECRET!,
     }
-)
+)*/
